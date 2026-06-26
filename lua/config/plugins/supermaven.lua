472da378dbdbb21abe1a8095efad62f28d372b9d
Konrad Miosga <konrad.miosga@gmail.com>
2026-06-26 11:11:56 +0200

    added supermaven plugin

---
+++ lua/config/plugins/supermaven.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"supermaven-inc/supermaven-nvim",
+		config = function()
+			require("supermaven-nvim").setup({
+				disable_inline_completion = false,
+				disable_keymaps = false,
+			})
+		end,
+	},
+}
